e57e0a888b0164238578fc3681e0d98be37d41f6
Nicolas Emery <Nicolas5150@gmail.com>
2017-02-10 00:05:46 -0500

    Text Messaging and Directions
    The site now allows the user to be texted right away of when he or she
    should be leaving, as well as a url to give them directions.
    
    The url is shortened using googles api for creating smaller url links.
    Doing this to compensate for the amount of characters a text message
    allows.
    
    Last step will be to have a modal for success on both the contact and
    submit page for user feedback.

--- js/events.js
+++ js/events.js
@@ -1,8 +1,7 @@
-// Date and time plugins.
 (function($){
-  // Popup for date
+  // Popup for date plugin.
   $( "#datepicker" ).datepicker();
-  // Select the current time or time needed.
+  // Select the current time or time needed plugin.
   $('#timepicker').timepicki({
     start_time: ["12", "00", "PM"],
     step_size_minutes:10});
@@ -45,38 +44,46 @@
         // Now allow user to hit the submit button to add to database.
         // http://stackoverflow.com/questions/195951/change-an-elements-class-with-javascript
         document.getElementById("event-add-btn").className = document.getElementById("event-add-btn").className.replace( /(?:^|\s)disabled(?!\S)/g , '' );
+
+        // Shorten the url of the destination by
+        // changing current and destination strings to url format (+ rather than space).
+        // ex). https://www.google.com/maps/dir/Tampa,+Florida/Tampa,+Florida/
+        // Then attach to the google url var for shortening.
+        var current = document.getElementById('autocomplete-current').value;
+        current = current.replace(/\s+/g, '+');
+        var destination = document.getElementById('autocomplete-destination').value;
+        destination = destination.replace(/\s+/g, '+');
+        var urlLink = "https://www.google.com/maps/dir/" +current+"/"+destination;
+
+        // Plugin that does the shortening.
+        jQuery.urlShortener.settings.apiKey='AIzaSyDP9wo3OPbTjnXug18mUwl0T68xMReaKdQ';
+        jQuery.urlShortener({
+          longUrl: urlLink,
+          success: function (shortUrl) {
+            document.getElementById('googleURL').value = shortUrl;
+          },
+          error: function(err)
+          {
+            alert(JSON.stringify(err));
+          }
+        });
       }
     })
 
+    // in-time button was clicked, check values one more time prior to text.
+    // If data is still there send out an ajax call to text the user.
+    $( "#event-add-btn" ).click(function() {
+        $.ajax({
+          type: 'POST',
+          url: 'http://sulley.cah.ucf.edu/~ni927795/in-time/php/text.php',
+          data: $('#event-form').serialize(),
+          success: function (data) {
+            alert(data);
+          }
+        });
+    });
 })(jQuery);
 
-  function addModal() {
-      // Get the modal
-    var modal = document.getElementById('myModal');
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-    }
-  }
 
   // https://developers.google.com/maps/documentation/javascript/places-autocomplete
   // This example displays an address form, using the autocomplete feature
@@ -152,10 +159,15 @@
         // arival time needed of the user.
         var now = moment(dateTime, "YYYY-MM-DD HH:mm");
         var olderDate = moment(now).subtract(travelTime, 'seconds').toDate();
+        // WORKING HERE
+        //document.getElementById('formated-time').value = olderDate;
         document.getElementById('depart').value = convertTo12Hour(olderDate);
 
         // Add values to screen for user to see
         document.getElementById('travel').value = secondsToHms(travelTime);
+
+        // Keep hidden a full object of the date and time the user should leave by.
+        document.getElementById("momentObj").value = olderDate;
       }
     });
   }
---
+++ php/text.php
@@ -0,0 +1,26 @@
+<?php
+		// Carrier and number to send text message to.
+		$number = htmlspecialchars($_POST['number']);
+		$carrier = htmlspecialchars($_POST['carrier']);
+		// Create one string for phone to email type purposes.
+		$numberAndCarrier = $number ."@". $carrier;
+
+		// Event details and time.
+		$events = htmlspecialchars($_POST['events']);
+		$momentObj = htmlspecialchars($_POST['momentObj']);
+
+		// Create a message for the user.
+		$subject = 'In-Time Alert';
+		$message = "Hey there, here's a notification letting you know to leave
+		for ". $events ." at ". $momentObj;
+
+		// Current Location, to destination. As well as gps route.
+		$urlLink = htmlspecialchars($_POST['googleURL']);
+
+		// Send off the text message
+		mail($numberAndCarrier, $message, $urlLink);
+
+		//echo $numberAndCarrier ."\n". $message ."\n". $urlLink ."\n". $momentObj;
+		echo $urlLink;
+
+?>
